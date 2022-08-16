7e96c9bdc7af3c5e0b7ae36825bdb1faf9d573c8
Noha Mohamed <nohamohamedali1993@gmail.com>
2022-08-16 06:35:41 +0200

    Add TextView

--- Sources/TwitterTextView/TwitterTextView.swift
+++ Sources/TwitterTextView/TwitterTextView.swift
@@ -1,6 +1,38 @@
-public struct TwitterTextView {
-    public private(set) var text = "Hello, World!"
+//
+//  CharchterCountsView.swift
+//  TwitterCounter
+//
+//  Created by Noha Mohamed on 13/08/2022.
+//
 
-    public init() {
+import UIKit
+
+
+public class TwitterTextView: UITextView , UITextViewDelegate{
+    //MARK: Outlets
+    
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        loadNibContent()
+    }
+    private func loadNibContent() {
+            let nib = UINib(nibName: "TwitterTextView", bundle: Bundle.module)
+            guard let view = nib.instantiate(withOwner: self, options: nil).first as? UIView else {return}
+            view.frame = bounds
+            addSubview(view)
+    }
+    public func textViewDidChange(_ textView: UITextView) {
+    }
+    public func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        return self.textLimit(existingText: textView.text,
+                              newText: text,
+                              limit: 280)
+    }
+    private func textLimit(existingText: String?,
+                           newText: String,
+                           limit: Int) -> Bool {
+        let text = existingText ?? ""
+        let isAtLimit = text.count + newText.count <= limit
+        return isAtLimit
     }
 }
